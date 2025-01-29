120fa56fd8e86c45f9b646279d44255d5712ee54
ruzica-glavas <ruzi99@hotmail.it>
2025-01-29 14:37:06 +0100

    add: all card in html

---
+++ main.js
@@ -0,0 +1,52 @@
+//URL per API: https://lanciweb.github.io/demo/api/pictures///
+
+const card= document.querySelector(`.card`)
+const container= document.querySelector(`.container`)
+
+
+
+   
+
+    fetch(`https://lanciweb.github.io/demo/api/pictures/`)
+        .then(response => response.json())
+        .then(data => {
+
+            // codice per far qualcosa con la risposta
+            console.log(data);
+
+            container.innerHTML="";
+
+            //aggiungere il ciclo for each per far passare tutte le carte
+
+            
+            data.forEach(element => {
+                console.log(element)
+
+                container.innerHTML += ` <div class="container">
+       
+            <div class="card" style="width: 18rem; display: block">
+                <img src="${element.url}" class="card-img-top" alt="Immagine della carta">
+                <div class="card-body">
+                <h5 class="card-title">${element.title}</h5>
+                <p class="card-text">${element.date}</p>
+                </div>
+            </div>
+        </div>`
+            });
+    
+    
+            
+            
+        })
+        .catch(error => {
+            // codice da eseguire in caso di errore
+            console.error(error);
+        });
+    
+       
+    
+
+       
+
+        
+     
